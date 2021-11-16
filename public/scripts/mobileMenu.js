54b20de0c78971fd204fe2212669cb04b0a53bbd
unknown <julianmontoya15@gmail.com>
2021-11-16 12:11:05 -0500

    Various Fix

--- public/scripts/mobileMenu.js
+++ public/scripts/mobileMenu.js
@@ -1,14 +1,17 @@
 const smallNavigation = document.querySelector(".small-navigation");
+const body = document.querySelector('body');
 const burgerMenu = document.querySelector(".burger-menu");
 burgerMenu.addEventListener('click', () => { 
     smallNavigation.classList.remove("invisible");
     smallNavigation.classList.add("visible");
+    body.classList.add("lock-screen");
 });
 
 const closeButton = document.querySelector("#small-menu-close-button");
 closeButton.addEventListener('click', () => {
     smallNavigation.classList.add("invisible");
     smallNavigation.classList.remove("visible");
+    body.classList.remove("lock-screen");
 });
 
 
